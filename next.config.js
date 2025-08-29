d68d40655989a01dde45ef9d52609f459852062c
Appycrew1 <appycrew1@gmail.com>
2025-08-29 21:03:29 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: { optimizePackageImports: ["@supabase/supabase-js"] }
+};
+module.exports = nextConfig;
