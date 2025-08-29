f299a634f71b6836dbf1d59543856b689318c07b
Appycrew1 <appycrew1@gmail.com>
2025-08-29 21:09:46 +0100

    Create getLogo.js

---
+++ utils/getLogo.js
@@ -0,0 +1,7 @@
+export function getLogo(p = {}) {
+  if (p.logo) return p.logo;
+  try {
+    const u = new URL(p.website || "");
+    return `https://www.google.com/s2/favicons?domain=${u.hostname}&sz=128`;
+  } catch { return ""; }
+}
