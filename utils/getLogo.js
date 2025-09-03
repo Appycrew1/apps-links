a2393abd94108ca2d0b1f96f2861338b1604ba65
Appycrew1 <appycrew1@gmail.com>
2025-09-03 22:59:13 +0100

    Delete utils directory

--- utils/getLogo.js
+++
@@ -1,8 +0,0 @@
-// utils/getLogo.js
-export function getLogo(p = {}) {
-  if (p.logo) return p.logo;
-  try {
-    const u = new URL(p.website || "");
-    return `https://www.google.com/s2/favicons?domain=${u.hostname}&sz=128`;
-  } catch { return ""; }
-}
